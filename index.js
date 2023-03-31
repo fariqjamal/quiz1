d9d782678d2a025917a40388316cc85cad531712
Your Name <you@example.com>
2023-03-31 11:20:32 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+//object 
+let user = {
+    name: "fariq",
+    faculty: "fkekk",
+    phone: {
+        office: "123456",
+        mobile: "456789"
+        },
+    email: "fariqjamal8@gmail.com"
+}
+console.log( user.phone.mobile )
